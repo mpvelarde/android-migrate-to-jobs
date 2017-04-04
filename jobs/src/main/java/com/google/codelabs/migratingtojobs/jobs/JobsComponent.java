f838fbae2a02c6bd1c9b6d2d86d8874a6813ca64
Maria Paz Velarde <mariapaz.velarde@gmail.com>
2017-04-04 15:34:24 +0100

    Added JobsComponent and JobsGlobalState

---
+++ jobs/src/main/java/com/google/codelabs/migratingtojobs/jobs/JobsComponent.java
@@ -0,0 +1,14 @@
+package com.google.codelabs.migratingtojobs.jobs;
+
+import com.google.codelabs.migratingtojobs.shared.AppModule;
+import com.google.codelabs.migratingtojobs.shared.RootComponent;
+
+import dagger.Component;
+
+import javax.inject.Singleton;
+
+@Singleton
+@Component(modules = {AppModule.class})
+public interface JobsComponent extends RootComponent {
+    public void inject(JobsGlobalState globalState);
+}
---
+++ jobs/src/main/java/com/google/codelabs/migratingtojobs/jobs/JobsGlobalState.java
@@ -0,0 +1,51 @@
+package com.google.codelabs.migratingtojobs.jobs;
+
+import android.app.Application;
+
+import com.google.codelabs.migratingtojobs.shared.AppModule;
+import com.google.codelabs.migratingtojobs.shared.EventBus;
+import com.google.codelabs.migratingtojobs.shared.SharedInitializer;
+
+import javax.inject.Inject;
+
+public class JobsGlobalState {
+    private static JobsGlobalState sInstance;
+
+    public static JobsComponent get(Application app) {
+        if (sInstance == null) {
+            synchronized (JobsGlobalState.class) {
+                if (sInstance == null) {
+                    sInstance = new JobsGlobalState(app);
+
+                    sInstance.init();
+                }
+            }
+        }
+
+        return sInstance.get();
+    }
+
+    private final JobsComponent component;
+
+    @Inject
+    SharedInitializer sharedInitializer;
+
+    @Inject
+    EventBus bus;
+
+    private JobsGlobalState(Application app) {
+        component = DaggerJobsComponent.builder()
+                .appModule(new AppModule(app))
+                .build();
+
+        component.inject(this);
+    }
+
+    private void init() {
+        sharedInitializer.init();
+    }
+
+    public JobsComponent get() {
+        return component;
+    }
+}
